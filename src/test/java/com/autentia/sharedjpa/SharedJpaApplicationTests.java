98a9323b02bbf1cacb4bbf897ba49e295d0e06a4
Belen Marin <marinsanchezbelen@gmail.com>
2024-05-10 12:39:35 +0200

    .jar done

--- src/test/java/com/autentia/sharedjpa/SharedJpaApplicationTests.java
+++ src/test/java/com/autentia/sharedjpa/SharedJpaApplicationTests.java
@@ -3,7 +3,7 @@ package com.autentia.sharedjpa;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = SharedJpaApplicationTests.class)
 class SharedJpaApplicationTests {
 
 	@Test
